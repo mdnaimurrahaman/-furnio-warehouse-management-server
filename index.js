8dfa32675e353b2d6024a37ca1db820e144d52dc
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-06 10:23:08 +0600

    Create a get api to load all items

--- index.js
+++ index.js
@@ -1,34 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const { response } = require("express");
+require("dotenv").config();
 const port = process.env.PORT || 5000;
 const app = express();
-
 
 //middleware
 app.use(cors());
 app.use(express.json());
 
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kbjxh.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  console.log('my server is connected')
-  // perform actions on the collection object
-  client.close();
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
 });
 
+async function run() {
+  try {
+    await client.connect();
+    const itemCollection = client.db("furnioData").collection("item");
 
-app.get('/',(req, res)=>{
-    res.send('running my server')
-})
+    app.get("/item", async (req, res) => {
+      const query = {};
+      const cursor = itemCollection.find(query);
+      const items = await cursor.toArray();
+      res.send(items);
+    });
+  } finally {
+  }
+}
+run().catch(console.dir);
 
-app.listen(port,()=>{
-    console.log('Listening to port', port)
-})
+app.get("/", (req, res) => {
+  res.send("running my server");
+});
 
-//Mg409ZEnHKN6GFHf
-//furnioUser
+app.listen(port, () => {
+  console.log("Listening to port", port);
+});

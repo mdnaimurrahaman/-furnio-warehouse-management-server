1b50b0a062fc49568694d690d56744280712c825
mdnaimurrahaman <mdnaimurrahaman202@gmail.com>
2022-05-06 12:55:47 +0600

    Create Api for single item detail and find item objects with item ids

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const cors = require("cors");
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const { response } = require("express");
 require("dotenv").config();
 const port = process.env.PORT || 5000;
@@ -28,6 +28,14 @@ async function run() {
       const items = await cursor.toArray();
       res.send(items);
     });
+
+    app.get('/item/:id',async(req, res)=>{
+        const id = req.params.id;
+        const query={_id: ObjectId(id)};
+        const item = await itemCollection.findOne(query);
+        res.send(item);
+    })
+
   } finally {
   }
 }
